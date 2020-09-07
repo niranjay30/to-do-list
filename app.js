1ba5b71e43d930dab2d60ed7e01d3c059eb63c2f
niranjay <niranjay.bhosale@gmail.com>
2020-09-07 12:18:11 +0530

    initial commit
    to-do-list

---
+++ app.js
@@ -0,0 +1,50 @@
+//jshint esversion:6
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const date = require(__dirname + "/date.js");
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+const items = [];
+const workItems = [];
+
+
+app.get("/", function (req, res){
+
+  const day = date.getDate();
+
+  res.render("list", {listTitle: day, newListItems: items});
+});
+
+
+app.post("/", function (req, res){
+  const item = req.body.newItem;
+
+  if(req.body.list==="Work List"){
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+
+  console.log(req.body.list);
+});
+
+
+app.get("/work", function(req, res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+
+
+app.listen(port, function(){
+  console.log("Running server on port " + port);
+})
